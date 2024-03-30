e8693928873ee2e2949971ac3c8af4e13bb59a08
initialrise <98199146+initialrise@users.noreply.github.com>
2024-03-30 21:14:34 +0545

    Minor bug fixes and refactoring

--- main.js
+++ main.js
@@ -1,51 +1,14 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-const { spawn } = require("child_process");
-const dotenv = require(dotenv).setup();
+require("dotenv").config();
 
 const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server);
+const ffmpegChildProcess = require("./utils/ffmpeg");
 
-const args = [
-  "-i",
-  "-",
-  "-c:v",
-  "libx264",
-  "-preset",
-  "ultrafast",
-  "-tune",
-  "zerolatency",
-  "-r",
-  `${25}`,
-  "-g",
-  `${25 * 2}`,
-  "-keyint_min",
-  25,
-  "-crf",
-  "25",
-  "-pix_fmt",
-  "yuv420p",
-  "-sc_threshold",
-  "0",
-  "-profile:v",
-  "main",
-  "-level",
-  "3.1",
-  "-c:a",
-  "aac",
-  "-b:a",
-  "128k",
-  "-ar",
-  128000 / 4,
-  "-f",
-  "flv",
-  `rtmp://a.rtmp.youtube.com/live2/${STREAM_KEY}`,
-];
-
-const ffmpegChildProcess = spawn("ffmpeg", args);
 ffmpegChildProcess.stdout.on("data", (data) => {
   console.log(`ffmpeg stdout: ${data}`);
 });
@@ -69,6 +32,7 @@ app.use(express.static(`${__dirname}/public`));
 app.get("/", (req, res) => {
   res.render("index.html");
 });
+
 server.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
